0bdd12d58a1f3cb0575d5ae5187ad7068d19fd1d
PUYUP <hellopuyup@gmail.com>
2020-10-18 21:00:43 +0700

    ...

--- backend/sputnik/celery.py
+++ backend/sputnik/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sputnik.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sputnik.settings.production')
 os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
 
 app = Celery('sputnik')
